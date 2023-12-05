3f092bc9db01395cf91a674f5328fd85e1f3ce06
rigterw <rigterwessel@gmail.com>
2023-12-05 20:13:00 +0100

    made a basic functions file

---
+++ 2023/basic_func/basicFunc.cpp
@@ -0,0 +1,50 @@
+#include "basicFunc.h"
+#include <fstream>
+#include <sstream>
+
+vector<string> splitString(string inputString, char splitChar){
+    vector<string> sections;
+    int lastCut = 0;
+
+    for (int i = 0; i < inputString.length(); i++){
+        if(inputString[i] == splitChar){
+
+            string cut = inputString.substr(lastCut + 1, i - lastCut -1);
+            sections.push_back(cut);
+            lastCut = i;
+        }
+    }
+    sections.push_back(inputString.substr(lastCut + 1));
+    return sections;
+}
+
+vector<string> splitWords(string inputString){
+    istringstream iss(inputString);
+
+    vector<string> words;
+
+    string word;
+    while(iss >> word){
+        words.push_back(word);
+    }
+
+    return words;
+}
+
+vector<string> loadFile(string fileName){
+    fstream file;
+    vector<string> fileLined;
+
+    file.open(fileName,ios::in);
+
+    if(file.is_open()){
+        string line;
+        while (getline(file, line))
+        {
+            fileLined.push_back(line);
+        }
+        file.close();
+    }
+
+    return fileLined;
+}
---
+++ 2023/basic_func/basicFunc.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <vector>
+#include <string>
+
+using namespace std;
+
+std::vector<std::string> splitString(std::string, char);
+std::vector<std::string> splitWords(std::string);
+std::vector<std::string> loadFile(std::string);

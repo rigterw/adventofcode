26fa223182149d16086ce1f87c07310682e2a3ba
Wessel <rigterwessel@gmail.com>
2024-12-08 00:46:06 +0100

    removed debugs

--- 2024/day7/2.ts
+++ 2024/day7/2.ts
@@ -1,15 +1,12 @@
 module.exports = {
     main: main
 };
-let debug = true
 export function main(input: string[]): any {
     let score = 0;
     for (let i = 0; i < input.length; i++) {
         const numbers = input[i].match(/\d+/g)?.map(Number);
-        debug = i == 1;
         if (IsSolveable([...numbers])) {
             score += numbers[0];
-            console.log(`${i} solved`);
         }
     }
 
@@ -31,28 +28,18 @@ function IsSolveable(numbers: number[]): boolean {
             let divider = Math.pow(3, j);
             let value = tritmap / divider;
 
-            if (debug)
-                console.log(`${value} div: ${divider} = ${tritmap}, / ${Math.pow(3, i)}`);
-
             if (2 <= value) {
                 score += numbers[i];
                 tritmap -= 2 * divider;
-                if (debug)
-                    console.log(`${oldScore} + ${numbers[i]} = ${score}`);
             } else if (1 <= value) {
                 score *= numbers[i];
                 tritmap -= divider;
-                if (debug)
-                    console.log(`${oldScore} x ${numbers[i]} = ${score}, ${oldScore * numbers[i]}`);
             } else {
                 score = Number('' + score + numbers[i]);
             }
         }
-        if (debug)
-            console.log(`${option} gave ${score}`);
+
         if (score == total) {
-            if (debug)
-                console.log("dkd")
             return true;
         }
     }

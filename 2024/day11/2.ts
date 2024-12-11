65b3ceb41321ecba718971034a7b62f82638d7d2
Wessel <rigterwessel@gmail.com>
2024-12-11 12:54:32 +0100

    optimized

--- 2024/day11/2.ts
+++ 2024/day11/2.ts
@@ -3,28 +3,42 @@ module.exports = {
 };
 const nBlinks = 75;
 const map = new Map<string, Map<number, number>>();
+let debug = false;
 
 export function main(input: string[]): any {
     let stones: string[] = input[0].split(" ");
     let nStones = 0;
     for (let stone = 0; stone < stones.length; stone++) {
+        debug = stone == 0;
         nStones += Blink(stones[stone], 0, nBlinks);
     }
-    return stones.length;
+    return nStones;
 }
 
-function searchMap(stone: string, depth: number, maxDepth: number) {
-    if (!map.has(stone)) {
-        const stoneMap = new Map<number, number>();
-    }
-}
-
-function Blink(stone: string, depth: number, maxDepth: number): number {
+function searchMap(stone: string, depth: number, maxDepth: number): number {
     if (depth >= maxDepth) {
         return 1;
     }
+    let stoneMap = map.get(stone);
+    let nStones = undefined;
+
+    if (stoneMap == undefined) {
+        stoneMap = new Map<number, number>();
+        map.set(stone, stoneMap);
+    } else {
+        nStones = stoneMap.get(depth);
+    }
+
+    if (nStones == undefined) {
+        nStones = Blink(stone, depth, maxDepth);
+        stoneMap.set(depth, nStones);
+    }
+    return nStones;
+}
+
+function Blink(stone: string, depth: number, maxDepth: number): number {
     if (stone == "0") {
-        return Blink("1", depth + 1, maxDepth);
+        return searchMap("1", depth + 1, maxDepth);
     }
     else if (stone.length % 2 == 0) {
         let slice1 = stone.slice(0, stone.length / 2);
@@ -35,11 +49,11 @@ function Blink(stone: string, depth: number, maxDepth: number): number {
         while (slice2[0] == "0" && slice2.length > 1)
             slice2 = slice2.substring(1);
 
-        let score = Blink(slice1, depth + 1, maxDepth);
-        score += Blink(slice2, depth + 1, maxDepth);
+        let score = searchMap(slice1, depth + 1, maxDepth);
+        score += searchMap(slice2, depth + 1, maxDepth);
 
         return score;
     } else {
-        return Blink(`${Number(stone) * 2024}`, depth + 1, maxDepth);
+        return searchMap(`${Number(stone) * 2024}`, depth + 1, maxDepth);
     }
 }

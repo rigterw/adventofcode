eaba5c329472fe45bc9292c5a77c3b68173861cd
Wessel <rigterwessel@gmail.com>
2024-12-12 12:14:27 +0100

    bugfixed part 1

--- 2024/day12/1.ts
+++ 2024/day12/1.ts
@@ -9,16 +9,17 @@ interface areaData {
     perimeter: number
 }
 
-
 export function main(input: string[]): any {
     const map = util.splitInput(input);
+    const visitedMap = util.deepCopy(map);
     let price: number = 0;
     for (let y = 0; y < map.length; y++) {
         for (let x = 0; x < map[y].length; x++) {
-            if (map[y][x] == "#")
+            if (visitedMap[y][x] == "#")
                 continue;
-
-            const area: areaData = SearchArea(map, x, y, map[y][x]);
+            let letter = map[y][x];
+            const area: areaData = SearchArea(map, visitedMap, x, y, map[y][x]);
+            // console.log(`${letter}: ${area.perimeter} * ${area.surface} = ${area.perimeter * area.surface}`);
             price += area.perimeter * area.surface;
         }
     }
@@ -26,31 +27,35 @@ export function main(input: string[]): any {
     return price;
 }
 
-export function SearchArea(map: string[][], x: number, y: number, prevValue: string): areaData {
+export function SearchArea(map: string[][], visitedMap: string[][], x: number, y: number, prevValue: string): areaData {
     if (y < 0 || x < 0 || y >= map.length || x >= map[y].length) {
         return { surface: 0, perimeter: 1 }
     }
-
     const value: string = map[y][x];
 
-    if (value == "#") {
+    if (visitedMap[y][x] == "#" && value == prevValue) {
         return { surface: 0, perimeter: 0 }
     }
 
     if (value != prevValue) {
         return { surface: 0, perimeter: 1 }
     }
+    visitedMap[y][x] = "#";
     const areaData: areaData = { surface: 1, perimeter: 0 };
-    for (let dy = -1; dy < 2; dy++) {
-        for (let dx = -1; dx < 2; dx++) {
-            if (dy == 0 && dx == 0)
-                continue;
-
-            const neighbourData: areaData = SearchArea(map, x + dx, y + dy, value);
-            areaData.surface += neighbourData.surface;
-            areaData.perimeter += neighbourData.perimeter;
+    for (let i = -2; i < 3; i++) {
+        if (i == 0) {
+            continue;
         }
+        let dx = 0;
+        let dy = 0;
+        if (i % 2 == 0) {
+            dy = i / 2;
+        } else {
+            dx = i;
+        }
+        const neighbourData: areaData = SearchArea(map, visitedMap, x + dx, y + dy, value);
+        areaData.surface += neighbourData.surface;
+        areaData.perimeter += neighbourData.perimeter;
     }
-
     return areaData;
 }

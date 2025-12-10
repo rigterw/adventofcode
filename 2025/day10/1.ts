16515c4ea53bd05dce798b5a2565966dcb052eb0
rigterw <rigterwessel@gmail.com>
2025-12-10 11:22:57 +0100

    2025 day 10 part 1 solved

--- 2025/day10/1.ts
+++ 2025/day10/1.ts
@@ -1,5 +1,13 @@
 export function main(input: string[]): any {
     const machines: Machine[] = convertInput(input);
+
+    let score = 0;
+
+    for (const machine of machines) {
+        score += findLowestSwitches(machine);
+    }
+
+    return score;
 }
 
 function convertInput(input: string[]): Machine[] {
@@ -9,11 +17,9 @@ function convertInput(input: string[]): Machine[] {
 
         const machineParts: string[] = machineConfig.split(" ");
 
-        const lampConfig: string[] = machineParts.shift().split("");
-        const lamps: boolean[] = [];
-        for (let i = 1; i < lampConfig.length - 1; i++) {
-            lamps.push(lampConfig[i] == "#");
-        }
+        let lampConfig: string = machineParts.shift();
+        lampConfig = lampConfig.replace("[", "");
+        lampConfig = lampConfig.replace("]", "");
 
         let joltageConfig: string = machineParts.pop();
         joltageConfig = joltageConfig.replace("{", "");
@@ -38,13 +44,94 @@ function convertInput(input: string[]): Machine[] {
             buttons.push(buttonList);
         }
 
-        machines.push({ lamps: lamps, buttons: buttons, joltage: joltages });
+        machines.push({ lamps: new Lamps(lampConfig), buttons: buttons, joltage: joltages });
     }
     return machines;
 }
 
+function findLowestSwitches(machine: Machine): number {
+    const cache: Set<string> = new Set<string>();
+    let initialConfig = "";
+    for (let i = 0; i < machine.lamps.length; i++) {
+        initialConfig += ".";
+    }
+
+    let allLamps: Lamps[] = [new Lamps(initialConfig)];
+    let depth = 0;
+    while (true) {
+        depth++;
+        const foundLamps: Lamps[] = [];
+
+        for (const lamp of allLamps) {
+            for (const button of machine.buttons) {
+                const newLamp = lamp.clone();
+
+                const newConfig = newLamp.switch(button);
+
+                if (cache.has(newConfig)) {
+                    continue;
+                }
+
+                if (newConfig == machine.lamps.toString()) {
+                    return depth;
+                }
+
+                cache.add(newConfig);
+                foundLamps.push(newLamp);
+
+
+
+            }
+        }
+
+        allLamps = foundLamps;
+    }
+}
+
 interface Machine {
-    lamps: boolean[];
+    lamps: Lamps;
     buttons: number[][];
     joltage: number[];
+}
+
+class Lamps {
+    private lamps: boolean[] = [];
+
+    get length() {
+        return this.lamps.length;
+    }
+
+    constructor(config: string | boolean[]) {
+        if (typeof config === "string") {
+
+            for (let char of config) {
+                this.lamps.push(char == "#");
+            }
+        } else {
+            this.lamps = config;
+        }
+    }
+
+    switch(buttons: number[]): string {
+        for (let change of buttons) {
+            this.lamps[change] = !this.lamps[change];
+        }
+
+        return this.toString();
+    }
+
+    clone() {
+        return new Lamps([...this.lamps]);
+    }
+
+    toString() {
+        let string = "";
+        for (let lamp of this.lamps) {
+            string += lamp ? "#" : ".";
+        }
+
+        return string;
+    }
+
+
 }
